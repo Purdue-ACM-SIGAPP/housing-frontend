d28e6aeadaa98891b6ffe98d22f8df1b247ce7d0
Adrian Maliackel <amaliack@purdue.edu>
2024-09-19 20:10:01 -0400

    Login skeleton code

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -1,1 +1,9 @@
 print("Hello World!!!!")
+
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import CustomMap from "../components/CustomMap";
+
+export default function LogIn() {
+    
+}

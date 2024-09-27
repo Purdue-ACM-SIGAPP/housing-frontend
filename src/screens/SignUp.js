29a01b752e2d5ec2612f46d8dbcd3402d0a84799
Jacques Simon <jsltimtiman@gmail.com>
2024-09-26 20:34:17 -0400

    test push

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export default function SignUp() {
     return (
-        <Text> "Hello World" </Text>
+        <Text> "Heldsnkjcdshbklhdsvbkhdagbjfkjldhsdkhdfslasdlhdsaladfsgksfdbkjhdsbjkhdfsblo World" </Text>
     )
 }

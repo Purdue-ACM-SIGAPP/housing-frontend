c54d92ca56202177f4c05738efa57df4a348e27d
Justin Puthenpurayil <jputhen@purdue.edu>
2024-10-24 20:04:10 -0400

    Add dropdown icon

--- src/components/DropdownBox.js
+++ src/components/DropdownBox.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
+import SvgComponent from "../../assets/SvgComponent";
 
 const DropdownBox = ({ question, answers }) => {
   const [showAnswers, setShowAnswers] = useState(false);
@@ -12,6 +13,10 @@ const DropdownBox = ({ question, answers }) => {
     <View style={styles.container}>
       <TouchableOpacity onPress={handleDrop}>
         <Text style={styles.question}>{question}</Text>
+        <Image
+          style={styles.image}
+          source={require("../../assets/chevron.png")}
+        />
       </TouchableOpacity>
       {showAnswers && <Text style={styles.answers}>{answers}</Text>}
     </View>
@@ -28,9 +33,18 @@ const styles = StyleSheet.create({
     marginRight: "5%",
     marginLeft: "5%",
   },
+  image: {
+    width: 28,
+    height: 28,
+    position: "absolute",
+    right: 0,
+    top: 0,
+  },
   question: {
     fontSize: 18,
     fontWeight: "bold",
+    position: "relative",
+    paddingRight: 32,
   },
   answers: {
     fontSize: 18,

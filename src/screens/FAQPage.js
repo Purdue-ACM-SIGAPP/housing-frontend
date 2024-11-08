997e718f65be6ef7d9c5fb46d6e40114aed086e5
sjkanz <kanzs@purdue.edu>
2024-11-07 20:41:38 -0500

    just some notes

--- src/screens/FAQPage.js
+++ src/screens/FAQPage.js
@@ -3,6 +3,10 @@ import { View, Text, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import DropdownBox from "../components/DropdownBox";
 
+//scrollable function?
+//boxes resize based on how big the description is - is that what we want? Or set box width?
+//potential thing on bottom for people to submit questions
+
 export default function FAQPage() {
   const navigation = useNavigation();
 
@@ -13,14 +17,16 @@ export default function FAQPage() {
     "Is off-campus housing also included in the app?",
     "How do I make an account?",
     "How do I report errors?",
+    "Test Question",
   ];
 
   const answers = [
     "Answer 1: To start a review...",
     "Answer 2: To upload an image/video...",
-    "Answer 3: Yes, off-campus housing is...",
+    "Answer 3: Yes, select off-campus housing is displayed in this app!",
     "Answer 4: To make an account, follow these steps...",
     "Answer 5: Report errors by clicking on...",
+    "let's see how long I can make this, does it autoformat?\n I wonder if backslash N works how long can I make this lalalalallalalalalala \n fjdsfjdhsakjfsdfjdkas \n",
   ];
 
   return (

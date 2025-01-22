e6db20d46d8b8c9c27c569a46eb523087c73468b
Soukerman <142164991+Soukerman@users.noreply.github.com>
2025-01-22 18:54:49 -0500

    Initial theme.js page for consistent styling accross pages

---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+const theme = {
+    colors: {
+        warning: "#DD5042",
+        success: "#77EBB7",
+        background: "#ffffff",
+        text: "#1D1D1D",
+        primary: "#065758",
+        secondary: "#CDDDDE",
+        secondaryDark: "#A5C2C4",
+        accent: "#BF6E65",
+        boilermakerGold: "#CFB991",
+    },
+    fontSizes: {
+        title: 40,
+        subtitle: 30,
+        text: 24,
+        subtext: 20,
+        input: 24,
+    },
+    cornerRadius: 25,
+};
+
+export default theme;

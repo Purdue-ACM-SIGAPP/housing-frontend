2b288f251170a4686bbf7d78f7b49d2e90be0013
Soukerman <142164991+Soukerman@users.noreply.github.com>
2025-01-23 19:43:53 -0500

    Implemented theme into VerificationPage.js proving it works

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,23 +1,25 @@
 const theme = {
-    colors: {
+    color: {
         warning: "#DD5042",
         success: "#77EBB7",
         background: "#ffffff",
         text: "#1D1D1D",
+        textLight: "#ffffff",
         primary: "#065758",
         secondary: "#CDDDDE",
-        secondaryDark: "#A5C2C4",
+        secondaryDark: "#A5C2C480",
         accent: "#BF6E65",
         boilermakerGold: "#CFB991",
+        shadow: "#00000040",
     },
-    fontSizes: {
+    text: {
         title: 40,
         subtitle: 30,
         text: 24,
         subtext: 20,
         input: 24,
     },
-    cornerRadius: 25,
+    borderRadius: 25,
 };
 
 export default theme;
